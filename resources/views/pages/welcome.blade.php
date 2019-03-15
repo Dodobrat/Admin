4cb9a6a0c2a9be9736f3c5d31c29438b4550473a
Deyan Bozhilov <dodobrat98@gmail.com>
2019-03-15 23:35:04 +0200

    datatables

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,9 +1,130 @@
 @extends('layouts.app')
 @section('content')
-
+    {{--<link href="https://cdn.datatables.net/buttons/1.2.2/css/buttons.dataTables.min.css" rel="stylesheet" type="text/css" />--}}
+    <div class="col-sm-12">
+        <div class="white-box">
+            <div class="table-responsive p-t-20 p-b-20 ">
+                <table id="myTable" class="table table-hover table-condensed table-bordered">
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Description</th>
+                        <th>Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Tiger Nixon</td>
+                        <td>System Architect</td>
+                        <td>Edinburgh</td>
+                        <td>61</td>
+                    </tr>
+                    <tr>
+                        <td>Garrett Winters</td>
+                        <td>Accountant</td>
+                        <td>Tokyo</td>
+                        <td>63</td>
+                    </tr>
+                    <tr>
+                        <td>Ashton Cox</td>
+                        <td>Junior Technical Author</td>
+                        <td>San Francisco</td>
+                        <td>66</td>
+                    </tr>
+                    <tr>
+                        <td>Cedric Kelly</td>
+                        <td>Senior Javascript Developer</td>
+                        <td>Edinburgh</td>
+                        <td>22</td>
+                    </tr>
+                    <tr>
+                        <td>Airi Satou</td>
+                        <td>Accountant</td>
+                        <td>Tokyo</td>
+                        <td>33</td>
+                    </tr><tr>
+                        <td>Tiger Nixon</td>
+                        <td>System Architect</td>
+                        <td>Edinburgh</td>
+                        <td>61</td>
+                    </tr>
+                    <tr>
+                        <td>Garrett Winters</td>
+                        <td>Accountant</td>
+                        <td>Tokyo</td>
+                        <td>63</td>
+                    </tr>
+                    <tr>
+                        <td>Ashton Cox</td>
+                        <td>Junior Technical Author</td>
+                        <td>San Francisco</td>
+                        <td>66</td>
+                    </tr>
+                    <tr>
+                        <td>Cedric Kelly</td>
+                        <td>Senior Javascript Developer</td>
+                        <td>Edinburgh</td>
+                        <td>22</td>
+                    </tr>
+                    <tr>
+                        <td>Airi Satou</td>
+                        <td>Accountant</td>
+                        <td>Tokyo</td>
+                        <td>33</td>
+                    </tr><tr>
+                        <td>Tiger Nixon</td>
+                        <td>System Architect</td>
+                        <td>Edinburgh</td>
+                        <td>61</td>
+                    </tr>
+                    <tr>
+                        <td>Garrett Winters</td>
+                        <td>Accountant</td>
+                        <td>Tokyo</td>
+                        <td>63</td>
+                    </tr>
+                    <tr>
+                        <td>Ashton Cox</td>
+                        <td>Junior Technical Author</td>
+                        <td>San Francisco</td>
+                        <td>66</td>
+                    </tr>
+                    <tr>
+                        <td>Cedric Kelly</td>
+                        <td>Senior Javascript Developer</td>
+                        <td>Edinburgh</td>
+                        <td>22</td>
+                    </tr>
+                    <tr>
+                        <td>Airi Satou</td>
+                        <td>Accountant</td>
+                        <td>Tokyo</td>
+                        <td>33</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 
 @endsection
 
 @section('js')
-
-@endsection
+    {{--<script src="https://cdn.datatables.net/buttons/1.2.2/js/dataTables.buttons.min.js"></script>--}}
+    {{--<script src="https://cdn.datatables.net/buttons/1.2.2/js/buttons.flash.min.js"></script>--}}
+    {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>--}}
+    {{--<script src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/pdfmake.min.js"></script>--}}
+    {{--<script src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/vfs_fonts.js"></script>--}}
+    {{--<script src="https://cdn.datatables.net/buttons/1.2.2/js/buttons.html5.min.js"></script>--}}
+    {{--<script src="https://cdn.datatables.net/buttons/1.2.2/js/buttons.print.min.js"></script>--}}
+    <script>
+        $(document).ready(function() {
+            $('#myTable').DataTable({
+                // "autoWidth": false,
+                // "responsive": false,
+                // "ordering": false,
+            });
+        });
+    </script>
+@append
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,44 +1,35 @@
 const mix = require('laravel-mix');
 
 mix.js([
-    'resources/plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.js',
-    'resources/plugins/bower_components/waypoints/lib/jquery.waypoints.js',
-    'resources/plugins/bower_components/counterup/jquery.counterup.min.js',
-    'resources/plugins/bower_components/nestable/jquery.nestable.js',
-    'resources/bootstrap/dist/js/bootstrap.min.js',
-    'resources/js/jquery.slimscroll.js',
-    'resources/js/waves.js',
-    'resources/js/custom.js',
-    'resources/js/jasny-bootstrap.js',
-    'resources/js/validator.js',
-    'resources/plugins/bower_components/toast-master/js/jquery.toast.js',
-    'resources/plugins/bower_components/custom-select/custom-select.min.js',
-    'resources/plugins/bower_components/bootstrap-select/bootstrap-select.min.js',
-    'resources/plugins/bower_components/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js',
-    'resources/plugins/bower_components/multiselect/js/jquery.multi-select.js',
-    'resources/plugins/bower_components/dropify/dist/js/dropify.min.js',
-    'resources/plugins/bower_components/dropzone-master/dist/dropzone.js',
-    'resources/plugins/bower_components/clockpicker/dist/jquery-clockpicker.min.js',
-    'resources/plugins/bower_components/jquery-asColorPicker-master/libs/jquery-asColor.js',
-    'resources/plugins/bower_components/jquery-asColorPicker-master/libs/jquery-asGradient.js',
-    'resources/plugins/bower_components/jquery-asColorPicker-master/dist/jquery-asColorPicker.min.js',
-    'resources/plugins/bower_components/bootstrap-datepicker/bootstrap-datepicker.min.js',
-    'resources/plugins/bower_components/timepicker/bootstrap-timepicker.min.js',
+        'resources/plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.js',
+        'resources/plugins/bower_components/waypoints/lib/jquery.waypoints.js',
+        'resources/plugins/bower_components/counterup/jquery.counterup.min.js',
+        'resources/plugins/bower_components/nestable/jquery.nestable.js',
+        'resources/bootstrap/dist/js/bootstrap.min.js',
+        'resources/js/jquery.slimscroll.js',
+        'resources/js/waves.js',
+        'resources/js/custom.js',
+        'resources/js/jasny-bootstrap.js',
+        'resources/js/validator.js',
+        'resources/plugins/bower_components/toast-master/js/jquery.toast.js',
+        'resources/plugins/bower_components/custom-select/custom-select.min.js',
+        'resources/plugins/bower_components/bootstrap-select/bootstrap-select.min.js',
+        'resources/plugins/bower_components/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js',
+        'resources/plugins/bower_components/multiselect/js/jquery.multi-select.js',
+        'resources/plugins/bower_components/dropify/dist/js/dropify.min.js',
+        'resources/plugins/bower_components/dropzone-master/dist/dropzone.js',
+        'resources/plugins/bower_components/clockpicker/dist/jquery-clockpicker.min.js',
+        'resources/plugins/bower_components/jquery-asColorPicker-master/libs/jquery-asColor.js',
+        'resources/plugins/bower_components/jquery-asColorPicker-master/libs/jquery-asGradient.js',
+        'resources/plugins/bower_components/jquery-asColorPicker-master/dist/jquery-asColorPicker.min.js',
+        'resources/plugins/bower_components/bootstrap-datepicker/bootstrap-datepicker.min.js',
+        'resources/plugins/bower_components/timepicker/bootstrap-timepicker.min.js',
     ], 'public/js/app.js')
 
-    .copy('resources/plugins/bower_components/summernote/dist/summernote.js', 'public/js/editor.js')
-
-    .less('resources/less/style-dark.less', 'public/css/style-dark.css')
-    .less('resources/less/style-light.less', 'public/css/style-light.css')
-
-    .copy('resources/js/html5shiv.js', 'public/js/html5shiv.js', true)
-    .copy('resources/js/respond.min.js', 'public/js/respond.min.js', true)
-    .copy('resources/plugins/bower_components/jquery/dist/jquery.min.js', 'public/js/jquery.min.js', true)
-    .copy('resources/plugins/bower_components/jquery-sparkline/jquery.sparkline.min.js', 'public/js/charts.js', true)
-    .copy('resources/plugins/bower_components/flag-icon-css/flags', 'public/flags', true)
-    .copy('resources/plugins/images', 'public/img', true)
-    .copy('resources/bootstrap/fonts', 'public/fonts', true)
-    .copy('resources/fonts', 'public/fonts', true)
+    .less('resources/less/dark/style.less', 'public/css/style-dark.css')
+    .less('resources/less/colors/default-dark.less', 'public/css/default-dark.css')
+    .less('resources/less/colors/default.less', 'public/css/default-light.css')
+    .less('resources/less/light/style.less', 'public/css/style-light.css')
 
     .styles([
         'resources/bootstrap/dist/css/bootstrap.min.css',
@@ -59,13 +50,22 @@ mix.js([
         'resources/plugins/bower_components/html5-editor/bootstrap-wysihtml5.css',
         'resources/plugins/bower_components/summernote/dist/summernote.css',
         'resources/plugins/bower_components/flag-icon-css/css/flag-icon.min.css',
+        'resources/plugins/bower_components/datatables/jquery.dataTables.min.css',
         'resources/css/animate.css',
         'resources/css/spinners.css',
         'resources/css/switches.css',
     ],  'public/css/add.css')
 
-    .styles('resources/less/colors/default-dark.less', 'public/css/default-dark.css')
-    .styles('resources/less/colors/default.less', 'public/css/default.css')
+    .copy('resources/plugins/bower_components/summernote/dist/summernote.js', 'public/js/editor.js', true)
+    .copy('resources/plugins/bower_components/datatables/jquery.dataTables.min.js', 'public/js/data-table.js', true)
+    .copy('resources/js/html5shiv.js', 'public/js/html5shiv.js', true)
+    .copy('resources/js/respond.min.js', 'public/js/respond.min.js', true)
+    .copy('resources/plugins/bower_components/jquery/dist/jquery.min.js', 'public/js/jquery.min.js', true)
+    .copy('resources/plugins/bower_components/jquery-sparkline/jquery.sparkline.min.js', 'public/js/charts.js', true)
+    .copy('resources/plugins/bower_components/flag-icon-css/flags', 'public/flags', true)
+    .copy('resources/plugins/images', 'public/img', true)
+    .copy('resources/bootstrap/fonts', 'public/fonts', true)
+    .copy('resources/fonts', 'public/fonts', true)
 
     .options({
         processCssUrls: false
